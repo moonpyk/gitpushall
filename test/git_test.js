dc5f9c4151db2877189196b1929118fb5e0b5594
Clément Bourgeois <moonpyk@gmail.com>
2013-07-03 00:13:45 +0200

    Unit tests for git.js

--- test/git_test.js
+++ test/git_test.js
@@ -9,14 +9,50 @@
 (function (exports) {
     "use strict";
 
-    var git = require('../lib/git.js');
+    var git = require('../lib/git.js'),
+        _ = require('lodash');
 
     exports.setUp = function (done) {
+        exports.valid_directory = process.cwd();
+        exports.invalid_directory = "/non/existant/path";
         done();
     };
 
     exports.noop = function (test) {
         test.ok(true);
         test.done();
+    };
+
+    exports.parseValidGitConfig = function (test) {
+        git.parseGitConfig(exports.valid_directory, function (err, parsed) {
+            test.ok(err === null);
+            test.ok(_.isObject(parsed));
+            test.done();
+        });
+    };
+
+    exports.parseInvalidGitConfig = function (test) {
+        git.parseGitConfig(exports.invalid_directory, function (err, parsed) {
+            test.ok(err !== null);
+            test.ok(parsed === null);
+            test.done();
+        });
+    };
+
+    exports.getValidRemotes = function (test) {
+        git.getRemotes(exports.valid_directory, function (err, remotes) {
+            test.ok(err === null);
+            test.ok(_.isArray(remotes));
+            test.ok(_.contains(remotes, 'origin'));
+            test.done();
+        });
+    };
+
+    exports.getInvalidRemotes = function (test) {
+        git.getRemotes(exports.invalid_directory, function (err, remotes) {
+            test.ok(err !== null);
+            test.ok(remotes === null);
+            test.done();
+        });
     };
 }(exports));
